45a7aade2104105dba9becd66fce3a048b7d69c5
jeong922 <hwajeong922@naver.com>
2023-01-14 21:01:39 +0900

    사이드바 메뉴 클릭시 해당 경로로 이동 추가

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { CgShoppingBag } from 'react-icons/cg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const MENU_TEXT_STYLE = 'py-3 cursor-pointer hover:opacity-70';
+const MENU_TEXT_STYLE = 'py-5 cursor-pointer hover:opacity-70';
 export default function SideMenu({ onClick }) {
+  const location = useLocation();
   return (
     <section className=''>
       <div className='h-[4.5rem] flex items-center '>
@@ -20,16 +21,36 @@ export default function SideMenu({ onClick }) {
           <span className='text-lg font-semibold'>J Shop</span>
         </Link>
       </div>
-      <ul className='text-sm'>
-        <li className={MENU_TEXT_STYLE}>
-          <span>WOMEN</span>
-        </li>
-        <li className={MENU_TEXT_STYLE}>
-          <span>MEN</span>
-        </li>
-        <li className={MENU_TEXT_STYLE}>
-          <span>ACC&SHOES</span>
-        </li>
+      <ul className='flex flex-col text-sm'>
+        <Link to='women' onClick={onClick}>
+          <span
+            className={`${
+              location.pathname === '/women' ? 'font-semibold' : 'font-normal'
+            } ${MENU_TEXT_STYLE}`}
+          >
+            WOMEN
+          </span>
+        </Link>
+        <Link to='men' className={MENU_TEXT_STYLE} onClick={onClick}>
+          <span
+            className={`${
+              location.pathname === '/men' ? 'font-semibold' : 'font-normal'
+            } ${MENU_TEXT_STYLE}`}
+          >
+            MEN
+          </span>
+        </Link>
+        <Link to='acc-shoes' onClick={onClick}>
+          <span
+            className={`${
+              location.pathname === '/acc-shoes'
+                ? 'font-semibold'
+                : 'font-normal'
+            } ${MENU_TEXT_STYLE}`}
+          >
+            ACC&SHOES
+          </span>
+        </Link>
       </ul>
     </section>
   );
